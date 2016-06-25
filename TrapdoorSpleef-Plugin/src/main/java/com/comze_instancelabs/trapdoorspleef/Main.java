cd7f04c286ff9ea8d7718893524a1558564a79c1
mepeisen <mep_eisen@web.de>
2016-06-25 20:42:47 +0200

    rework config strings

--- TrapdoorSpleef-Plugin/src/main/java/com/comze_instancelabs/trapdoorspleef/Main.java
+++ TrapdoorSpleef-Plugin/src/main/java/com/comze_instancelabs/trapdoorspleef/Main.java
@@ -22,6 +22,7 @@ import org.bukkit.material.TrapDoor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comze_instancelabs.minigamesapi.Arena;
+import com.comze_instancelabs.minigamesapi.ArenaConfigStrings;
 import com.comze_instancelabs.minigamesapi.ArenaSetup;
 import com.comze_instancelabs.minigamesapi.ArenaState;
 import com.comze_instancelabs.minigamesapi.MinigamesAPI;
@@ -66,7 +67,7 @@ public class Main extends JavaPlugin implements Listener {
 		if (!config.isSet("arenas")) {
 			return ret;
 		}
-		for (String arena : config.getConfigurationSection("arenas.").getKeys(false)) {
+		for (String arena : config.getConfigurationSection(ArenaConfigStrings.ARENAS_PREFIX).getKeys(false)) {
 			if (Validator.isArenaValid(plugin, arena, cf.getConfig())) {
 				ret.add(initArena(arena));
 			}
